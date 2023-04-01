75758c27fdf97f9790fd2ee00c0f3c6a0e1f88dc
Bernie FitzGerald <bernardf@live.com.au>
2023-04-01 15:49:59 +1100

    Theme WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import LiveGamesTable from "./LiveGamesTable";
 import NavBar from "./NavBar";
 import "./App.css";
+import { ThemeProvider } from "@mui/system";
+import theme from "./theme";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavBar />
       <LiveGamesTable />
-    </>
+      <Button color="primary" variant="contained">Primary</Button>
+      <Button color="secondary" variant="contained">Secondary</Button>
+    </ThemeProvider>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#2196f3',
+      main: '#000000',
     },
     secondary: {
       main: '#f50057',
